e2187189689cccb1303da9de3e4e0510da8d62f2
Gabe da Silveira <gabe@websaviour.com>
2010-01-20 22:21:59 -0800

    This is purely a Rails plugin, so get rid of the init.rb whose behavior varies between Rails versions, and move the setup into the base required file.
    Move the actual code into a subdir, Rails-style, so the main include only shows the plugin structure.

--- lib/tm_syntax_highlighting.rb
+++ lib/tm_syntax_highlighting.rb
@@ -1,81 +1,10 @@
-module TmSyntaxHighlighting
-  module Helper
-    def code(code, options = {})
-      options = TmSyntaxHighlighting.defaults.merge_with(options)
-      TmSyntaxHighlighting.defaults[:current_themes] << options[:theme]
-      options[:lang] = "plain_text" unless Uv.syntaxes.include?(options[:lang])
-      Uv.parse(code, "xhtml", options[:lang], options[:line_numbers], options[:theme])
-    end
+require 'rubygems'
+require 'uv'
 
-    def syntax_css(theme = nil)
-      themes = [theme || TmSyntaxHighlighting.defaults.options[:theme]].flatten
-      themes = (themes & TmSyntaxHighlighting.defaults[:current_themes]) if theme.nil? && TmSyntaxHighlighting.defaults[:current_themes]
-      themes.uniq.collect do |theme|
-        stylesheet_link_tag "syntax/#{theme}"
-      end.join("\n")
-    end
-  end
-  
-  module Controller
-    def self.extended(base)
-      base.after_filter :reset_syntax_css_includes
-      base.send(:include, InstanceMethods)
-      TmSyntaxHighlighting.defaults.reset_current_themes
-    end
-    
-    module ClassMethods
-    end
-    
-    module InstanceMethods
-      def reset_syntax_css_includes
-        TmSyntaxHighlighting.defaults.reset_current_themes
-      end
-    end
-  end
+# takes about a second to this, if we don't do it now, then the first call to highlight will take a little longer
+Uv.init_syntaxes 
 
-  def self.defaults
-    Config
-  end
+require 'tm_syntax_highlighting/base'
 
-  def self.defaults=(options_hash)
-    options_hash.each do |key, value|
-      self.defaults.send("#{key}=", value)
-    end
-  end
-
-  class Config
-    @@attributes = [:theme, :lang, :line_numbers, :current_themes]
-    cattr_accessor @@attributes
-
-    self.line_numbers = true
-    class << self
-      def [](key)
-        self.send(key)
-      end
-
-      def []=(key, value)
-        self.send("#{key}=", value)
-      end
-
-      def options
-        opts = {}
-        @@attributes.each do |attribute|
-          opts[attribute] = self.send(attribute)
-        end
-        opts
-      end
-
-      def merge_with(opts)
-        opts = self.options.merge(opts)
-        if opts[:theme].is_a?(Array)
-          opts[:theme] = opts[:theme][rand(opts[:theme].size)]
-        end
-        opts
-      end
-      
-      def reset_current_themes
-        self.current_themes = []
-      end
-    end
-  end
-end
+ActionView::Base.send(:include, TmSyntaxHighlighting::Helper)
+ActionController::Base.send(:extend, TmSyntaxHighlighting::Controller)
